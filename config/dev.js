084d2636b28cf05a19d56fc1d33e91207c034fbe
Careytech <careythetech@gmail.com>
2020-09-02 15:31:06 -0400

    db and server

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://careytheman7:<boo777>@cluster0.tlgup.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://careytheman7:boo777@cluster0.tlgup.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+const config = require("./config/key");
+// var cookieParser = require('cookie-parser')
+const mongoose = require("mongoose");
+// app.use(cookieParser())
+
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 
 
@@ -8,19 +18,22 @@
 
 
 
+if (process.env.NODE_ENV === "production") {
+
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    // index.html for all page routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
-
-
-
-
-app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-})
 
 
 
 const port = process.env.Port || 3000
-app.listen(post, () => {
+app.listen(port, () => {
     console.log(`Server Running at ${port}`)
 })
