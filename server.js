ab527f94df0c617539f0ad1824cb3dd1b536dad6
Careytech <careythetech@gmail.com>
2020-09-02 15:55:42 -0400

    Made Views

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const config = require("./config/key");
 const mongoose = require("mongoose");
 // app.use(cookieParser())
 
+
+// Mongo Datebase
 const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+// Routes
+    // app.use('/api/users', require('./routes/users'));
+    // app.use('/api/comment', require('./routes/comment'));
+    // app.use('/api/like', require('./routes/like'));
+    // app.use('/api/favorite', require('./routes/favorite'));
 
-
-
-
-
-
-
-
-
+// Client Connections
 if (process.env.NODE_ENV === "production") {
 
     // Set static folder
@@ -28,11 +28,7 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
-
-
-
-
-
+// Server Listen
 const port = process.env.Port || 3000
 app.listen(port, () => {
     console.log(`Server Running at ${port}`)
